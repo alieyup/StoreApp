adda66a9f4558864fd1967f2bab880a8e142682a
AB129050 <AB129050@AB60013-129050>
2023-09-11 16:51:07 +0300

    gün sonu 4.4 ders sonu

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/btk", () => "mrb World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
